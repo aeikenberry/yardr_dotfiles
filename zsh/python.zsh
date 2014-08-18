bfa27472929b797ebf301ac1acbc83854dbba1c8
Aaron Eikenberry <aeikenberry@gmail.com>
2014-08-18 09:45:38 -0500

    removes ript-specific aliases

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -18,20 +18,3 @@ alias syncdb='python manage.py syncdb'
 alias dtest='python manage.py test'
 alias dshell='python manage.py shell'
 alias celeryd='python manage.py celery worker --loglevel=info'
-
-# RIPT STUFF
-alias ript="cd $PROJECT_HOME/ript && workon ript"
-alias rrun="ript && python local_manage.py runserver"
-alias rtest="ript && python local_manage.py test"
-alias rshell="ript && python local_manage.py shell"
-alias rmigrate="ript && python local_manage.py migrate"
-alias maus="cd $PROJECT_HOME/reporting && workon maus"
-alias lrun="python local_manage.py runserver"
-alias ltest="python local_manage.py test --settings=tests.settings"
-alias lshell="python local_manage.py shell"
-alias lmigrate="python local_manage.py migrate"
-
-# Heroku
-alias pshell='heroku run python manage.py shell --remote production'
-alias sshell='heroku run python manage.py shell --remote staging'
-
